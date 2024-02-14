ebaaca27441e5b976c58444af28ee333338ccfcf
SergachevYuri <sergachev.yuri@gmail.com>
2024-02-14 13:44:58 +0900

    add tags in Journal

--- journal/admin.py
+++ journal/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 
-from .models import Journal
+from .models import Journal, Tag
 
 # Register your models here.
 
 class JournalAdmin(admin.ModelAdmin):
-    list_display = ('department', 'username', 'description', 'create_dt', 'tags')
+    list_display = ('department', 'username', 'description', 'create_dt', 'get_tags')
     search_fields = ('department', 'username', 'description', 'create_dt', 'tags')
     list_filter = ('department', 'username', 'description', 'create_dt', 'tags')
 
+    def get_tags(self, obj):
+        return ", ".join([p.name for p in obj.tags.all()])
 
+
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name', )
+
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Journal, JournalAdmin)
--- journal/models.py
+++ journal/models.py
@@ -3,23 +3,6 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
-
-
-# Create your models here.
-'''
-Технический журнал.
-Сам журнал с данными.
-
-Table - journal
-date
-department
-name
-description
-solution
-tags
-
-'''
-
 class Journal(models.Model):
     class Meta:
         db_table = 'journal'
@@ -30,9 +13,23 @@ class Journal(models.Model):
     username = models.CharField(max_length=255, verbose_name="Фамилия Имя Отчество")
     description = models.TextField(verbose_name="Описание проблемы")
     solution = models.TextField(verbose_name="Решение проблемы")
-    tags = models.CharField(max_length=100, verbose_name="Теги")
+    #tags = models.CharField(max_length=100, verbose_name="Теги")
+    tags = models.ManyToManyField("Tag", verbose_name="Теги")
     create_dt = models.DateTimeField(default=timezone.now, verbose_name="Дата добавления")
+    update_dt = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
 
     def __str__(self):
         return f"{self.create_dt} {self.department} {self.username}"
     
+
+class Tag(models.Model):
+    class Meta:
+        db_table = 'tag'
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    name = models.CharField(max_length=100, verbose_name="Название тега")
+
+    def __str__(self):
+        return self.name
