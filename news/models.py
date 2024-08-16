a70c1795963bf1df35a7c4d4897906bed8fe7e4e
SYuri <Sergachev.yuri@gmail.com>
2024-08-16 10:50:32 +0900

    Migrate NEWS Model

--- news/models.py
+++ news/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 # Create your models here.
 
+class NewsCategory(models.Model):
+    class Meta:
+        db_table = "news_category"
+        verbose_name = "Категория новостей"
+        verbose_name_plural = "Категория новостей"
+
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название категории")
+
+    def __str__(self):
+        return self.name
+
 
 class News(models.Model):
     class Meta:
@@ -14,7 +25,7 @@ class News(models.Model):
     title_name = models.CharField(max_length=255, unique=True, verbose_name="Заголовок")
     describtion = models.CharField(max_length=255, verbose_name="Описание")
     content = models.TextField(default='', verbose_name="Содержание")
-    category = models.ForeignKey(NewsCategory, verbose_name="Категория")
+    category = models.ForeignKey(NewsCategory, on_delete=models.CASCADE, verbose_name="Категория")
     date_added = models.DateTimeField(default=timezone.now, verbose_name="Дата добавления")
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
 
@@ -22,15 +33,5 @@ class News(models.Model):
         return self.title_name
 
 
-class NewsCategory(models.model):
-    class Meta:
-        db_table = "news_category"
-        verbose_name = "Категория новостей"
-        verbose_name_plural = "Категория новостей"
-
-    name = models.CharField(max_length=100), unique=True, verbose_name="Название категории"
-
-    def __str__(self):
-        return self.name
 
     
