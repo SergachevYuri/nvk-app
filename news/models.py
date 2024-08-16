5a648af170f3dd6b8d1fb19826d07134317e2bbd
SYuri <Sergachev.yuri@gmail.com>
2024-08-16 11:55:41 +0900

    News

--- news/models.py
+++ news/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from django.conf import settings
 
 # Create your models here.
 
@@ -29,14 +28,7 @@ class News(models.Model):
     content = models.TextField(default='', verbose_name="Содержание")
     category = models.ForeignKey(NewsCategory, on_delete=models.CASCADE, verbose_name="Категория")
     date_added = models.DateTimeField(default=timezone.now, verbose_name="Дата добавления")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь", default=None, editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
 
     def __str__(self):
         return self.title_name
-
-    def save(self, *args, **kwargs):
-        # Check if the object is being created (not updated)
-        if not self.pk:
-            # Set the author to the currently logged-in user
-            self.author = User.objects.get(pk=settings.AUTH_USER_MODEL)
-        super(News, self).save(*args, **kwargs)
